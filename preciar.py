9bc4c04e22870af52b55b8bbdff657a169135c3e
Nicolas Pauer <89319674+NicoPauer@users.noreply.github.com>
2024-12-01 21:05:31 -0300

    Actualizacion definitiva del archivo

--- preciar.py
+++ preciar.py
@@ -1,26 +1,56 @@
 #!/usr/bin/python3
-import sys
+import sys, math
 
-print('\n\tCalcula el pasaje de un precio en USD (dólares estadounidenses) por cantidad de libras(LB) a precio en ARS (pesos argentinos) por kilogramo (Kg).\n\n')
-# Obtengo datos validos
-precio = float(sys.argv[1])
-# Valido precio
-while (precio < 1):
-    precio = float(input('\n\tEl precio en USD debe ser mayor o igual a 1: '))
+def truncado(num:float, lugares:int) -> float:
+    '''
+        Devuelve un numero real truncado a n
+        lugares después de la parte entera.
+    '''
+    potencia = (10 ** lugares)
+    return (math.trunc(num * potencia) / potencia)
 
-libras_compradas = float(sys.argv[2])
-# Valido cantidad de libras (puede ser 0.25, cuarto de libra pero no cero ni negativa)
-while (libras_compradas <= 0):
-    libras_compradas = float(input('\nIngrese cuantas libras se pueden comprar con USD %.2f (mas de cero): ' % precio))
-# Obtengo precio de ese algo en dolares por libra
-resultado_dolares_libra = (precio / libras_compradas)
-# Paso precio en dolares a pesos con equivalencia variable que voy actualizando semanalmente
-pesos_equivalentes = float(sys.argv[3])
-# Convierto a pesos
-precio = (precio * pesos_equivalentes)
-# Convierto cantidad de libras que se pueden comprar a ese precio de algo a kilogramo
-masa_kilos = (libras_compradas * 0.453592)
-# Obtengo el precio en pesos por kilo a partir del precio de dolares por libras
-resultado_pesos_kilos = (precio / masa_kilos)
-# Muestro resultado
-print('\n\n\tEl resultado de %.2f USD/LB son %.2f ARS/Kg (a dolar de %.2f ARS).\n' % (resultado_dolares_libra, resultado_pesos_kilos, pesos_equivalentes))
+def peso(medida:float, equivalencia:float) -> float:
+    '''
+        Convierte una medida expresada en otra unidad con
+        su equivalencia a la unidad de peso buscada
+    '''
+    return (medida * equivalencia)
+
+def divisa(precio:float, tipo_de_cambio:float) -> float:
+    '''
+        Convierte un precio expresado en una divisa con
+        su tipo de cambio a otra.
+    '''
+    return truncado((precio * tipo_de_cambio), 2)
+
+def argumentos(posicion:int) -> float:
+    '''
+        Obtiene argumento real en una posicion n
+        de la linea de comandos.
+    '''
+    return truncado(float(sys.argv[posicion]), 2)
+
+def argu_text(posicion:int) -> str:
+    '''
+        Obtiene argumentos de tipo texto
+        en una posicion n.
+    '''
+    return str(sys.argv[posicion])
+# Implemento algoritmo
+unidad_peso_entrada = argu_text(1)
+
+unidad_peso_salida = argu_text(2)
+
+precio = argumentos(3)
+
+pesa = argumentos(4)
+
+equivalencia_pesaje = argumentos(5)
+
+equivalencia_divisas = argumentos(6)
+
+divisa_entrada = argu_text(7)
+
+divisa_salida = argu_text(8)
+# Muestro el resultado
+print(f'\n\t{pesa} {unidad_peso_entrada} a {divisa_entrada} {precio} son {divisa_salida} {truncado((divisa(precio, equivalencia_divisas) / peso(pesa, equivalencia_pesaje)), 2)} por cada {unidad_peso_salida} a {divisa_salida} {equivalencia_divisas} por cada {divisa_entrada}.\n')
